36f9a23e801a17c9e44a80dee90f6b2c79b18fce
KZL <lz3450@outlook.com>
2024-02-24 23:47:26 -0600

    setup

---
+++ ros2_setup.bash
@@ -0,0 +1,8 @@
+ROS2_WS="/home/kzl/Projects/ros2_ws"
+
+ROS2_SETUP="$ROS2_WS/ros2_humble/install/local_setup.bash"
+if [[ -f "$ROS2_SETUP" ]]; then
+    # export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+    echo "ros2_humble (bash)"
+    source "$ROS2_SETUP"
+fi
