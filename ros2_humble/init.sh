83be293fd9a3b8f57e7233d305d5b7e1642a320a
KZL <lz3450@outlook.com>
2024-06-11 19:51:02 -0500

    ros2

--- ros2_humble/init.sh
+++ ros2_humble/init.sh
@@ -4,17 +4,17 @@
 
 set -e
 
-sudo apt update
-sudo apt upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
 
-sudo apt install -y curl
+sudo apt-get install -y curl
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-sudo apt update
+sudo apt-get update
 
-sudo apt install -y \
+sudo apt-get install -y \
     python3-pip \
     ros-dev-tools
 
@@ -24,7 +24,7 @@ vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos
 sudo rosdep init || :
 rosdep update
 
-sudo apt install -y \
+sudo apt-get install -y \
     libacl1-dev \
     libasio-dev \
     libtinyxml2-dev \
