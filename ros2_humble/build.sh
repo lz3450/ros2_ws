654fea268df2a43b65a467c108fd3a88fccd0189
KZL <lz3450@outlook.com>
2024-06-15 02:07:20 -0500

    ros2

--- ros2_humble/build.sh
+++ ros2_humble/build.sh
@@ -2,13 +2,21 @@
 
 set -e
 
-mkdir -p src
-vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
-
-export PATH="/usr/bin:/opt/llvm-project/bin"
-export MAKEFLAGS="-j $(nproc)"
-colcon build \
-    --symlink-install \
+COMMON_OPTIONS=(
+    --symlink-install
     --parallel-workers $(nproc)
+    # --continue-on-error
+    # --packages-skip-build-finished
+)
 
-# colcon build --build-base build-merge --install-base install-merge --merge-install --symlink-install --parallel-workers 2 --continue-on-error --packages-skip-build-finished
+export MAKEFLAGS="-j $(nproc)"
+
+colcon build \
+    "${COMMON_OPTIONS[@]}"
+
+# colcon build \
+#     --build-base build-merge \
+#     --install-base install-merge \
+#     --merge-install \
+#     "${COMMON_OPTIONS[@]}"
+
