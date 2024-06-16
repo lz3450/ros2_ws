8a5ccaeb95c4b967b015187df63838b382dfc951
KZL <lz3450@outlook.com>
2024-06-15 19:11:24 -0500

    nav2

--- nav2/init.sh
+++ nav2/init.sh
@@ -8,7 +8,7 @@ set -e
 mkdir -p src
 vcs import --input deps.repos src
 
-source ../ros2_setup.bash
+source ../ros2_setup.sh
 
 rosdep install --from-paths src --ignore-src -s | sed '1d' | awk '{print $5}' | sort -n > rosdep.txt
 grep -v 'ros-' rosdep.txt | xargs sudo apt-get install -s | grep "^Inst" | awk '{print $2}' | sort -n > rosdep-pkgs.txt
