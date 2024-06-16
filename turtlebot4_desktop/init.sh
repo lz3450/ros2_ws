bab96ae76c27cb06d042ec2cee49d2f1df3cfd07
KZL <lz3450@outlook.com>
2024-06-15 20:57:03 -0500

    turtlebot4_desktop

--- turtlebot4_desktop/init.sh
+++ turtlebot4_desktop/init.sh
@@ -10,6 +10,6 @@ vcs import --input deps.repos src
 
 source ../ros2_setup.sh
 
-rosdep install --from-paths src --ignore-src -s | awk '{print $5}' | sed -E '/^\s*$/d' | sort -n > rosdep.txt
+rosdep install --reinstall --from-paths src --ignore-src -s | awk '{print $5}' | sed -E '/^\s*$/d' | sort -n > rosdep.txt
 grep -v 'ros-' rosdep.txt | xargs sudo apt-get install -s | grep "^Inst" | awk '{print $2}' | sort -n > rosdep-pkgs.txt
 grep -v 'ros-' rosdep.txt | xargs sudo apt-get install -y
