d16b6d5c92f603292722009151ceaa118297a323
KZL <lz3450@outlook.com>
2024-06-17 00:06:08 -0500

    tb4_desktop

--- turtlebot4_desktop/init.sh
+++ turtlebot4_desktop/init.sh
@@ -2,14 +2,18 @@
 
 set -e
 
-# sudo apt-get update
-# sudo apt-get upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
 
 mkdir -p src
 vcs import --input deps.repos src
 
 source ../ros2_setup.sh
 
-rosdep install --reinstall --from-paths src --ignore-src -s | awk '{print $5}' | sed -E '/^\s*$/d' | sort -n > rosdep.txt
+# rosdep install \
+#     --reinstall \
+#     --from-paths src \
+#     --ignore-src \
+#     -s | awk '{print $5}' | sed -E '/^\s*$/d' | sort -n > rosdep.txt
 grep -v 'ros-' rosdep.txt | xargs sudo apt-get install -s | grep "^Inst" | awk '{print $2}' | sort -n > rosdep-pkgs.txt
 grep -v 'ros-' rosdep.txt | xargs sudo apt-get install -y
