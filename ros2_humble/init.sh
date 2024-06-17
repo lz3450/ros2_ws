cd0d3745b46c0727b3b5ad0c78e7b829ada61e1d
KZL <lz3450@outlook.com>
2024-06-17 00:37:08 -0500

    ros2

--- ros2_humble/init.sh
+++ ros2_humble/init.sh
@@ -23,15 +23,5 @@ sudo apt-get install -y \
 mkdir -p src
 vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
 
-sudo rosdep init || :
-rosdep update
-
-# rosdep install \
-#     --reinstall \
-#     --from-paths src \
-#     --ignore-src \
-#     --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers" \
-#     -s \
-#     | awk '{print $5}' | sed -E '/^\s*$/d' | sort -n > rosdep.txt
 xargs -a rosdep.txt sudo apt-get install -s | grep "^Inst" | awk '{print $2}' | sort -n > rosdep-pkgs.txt
 xargs -a rosdep.txt sudo apt-get install -y
---
+++ ros2_humble/update-rosdep.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+set -e
+
+mkdir -p src
+vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
+
+sudo rosdep init || :
+rosdep update
+
+rosdep install \
+    --reinstall \
+    --from-paths src \
+    --ignore-src \
+    --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers" \
+    -s \
+    | awk '{print $5}' | sed -E '/^\s*$/d' | sort -n > rosdep.txt
