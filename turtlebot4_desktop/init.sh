66ea9595f687d94086aa148ba57706d29bfbe2f7
KZL <lz3450@outlook.com>
2024-06-17 00:17:14 -0500

    tb4_desktop

--- turtlebot4_desktop/init.sh
+++ turtlebot4_desktop/init.sh
@@ -7,13 +7,19 @@ sudo apt-get upgrade -y
 
 mkdir -p src
 vcs import --input deps.repos src
+rm -rf src/create3_sim/irobot_create_gazebo
+rm -rf src/create3_sim/irobot_create_ignition
+rm -rf src/create3_sim/irobot_create_common/irobot_create_common_bringup
+rm -rf src/create3_sim/irobot_create_common/irobot_create_control
+rm -rf src/create3_sim/irobot_create_common/irobot_create_nodes
+rm -rf src/create3_sim/irobot_create_common/irobot_create_toolbox
 
 source ../ros2_setup.sh
 
-# rosdep install \
-#     --reinstall \
-#     --from-paths src \
-#     --ignore-src \
-#     -s | awk '{print $5}' | sed -E '/^\s*$/d' | sort -n > rosdep.txt
+rosdep install \
+    --reinstall \
+    --from-paths src \
+    --ignore-src \
+    -s | awk '{print $5}' | sed -E '/^\s*$/d' | sort -n > rosdep.txt
 grep -v 'ros-' rosdep.txt | xargs sudo apt-get install -s | grep "^Inst" | awk '{print $2}' | sort -n > rosdep-pkgs.txt
 grep -v 'ros-' rosdep.txt | xargs sudo apt-get install -y
