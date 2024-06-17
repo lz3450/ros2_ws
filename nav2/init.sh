bf41105fe26abf6f2537e3d7587bca1ea60453da
KZL <lz3450@outlook.com>
2024-06-17 01:26:41 -0500

    nav2

--- nav2/init.sh
+++ nav2/init.sh
@@ -11,9 +11,7 @@ sudo apt-get upgrade -y
 
 mkdir -p src
 vcs import --input deps.repos src
-rm -rf src/navigation2/nav2_system_tests
-
-source ../ros2_setup.sh
+xargs -a unnecessary_ros2_pkgs.txt -I {} rm -rf src/{}
 
 grep -v 'ros-' rosdep.txt | xargs sudo apt-get install -s | grep "^Inst" | awk '{print $2}' | sort -n > rosdep-pkgs.txt
 grep -v 'ros-' rosdep.txt | xargs sudo apt-get install -y
