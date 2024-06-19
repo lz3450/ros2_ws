06b713e77ae0c96dab64167fa20f837c8998dff2
KZL <lz3450@outlook.com>
2024-06-19 12:41:05 -0600

    ros2

--- ros2_humble/init.sh
+++ ros2_humble/init.sh
@@ -20,8 +20,13 @@ sudo apt-get install -y \
     python3-pip \
     ros-dev-tools
 
-mkdir -p src
-vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
+if [[ ! -d "src" ]]; then
+    mkdir -p src
+    vcs import --force --shallow --recursive --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
+    if [[ -f "unnecessary_ros2_pkgs.txt" ]]; then
+        xargs -a unnecessary_ros2_pkgs.txt -I {} rm -rf src/{}
+    fi
+fi
 
 xargs -a rosdep.txt sudo apt-get install -s | grep "^Inst" | awk '{print $2}' | sort -n > rosdep-pkgs.txt
 xargs -a rosdep.txt sudo apt-get install -y
