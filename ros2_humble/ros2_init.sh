ba380ff39bf5c22403b4c38344dcd9fa4f6ce432
KZL <lz3450@outlook.com>
2024-06-30 18:57:10 +0000

    ros2

--- ros2_humble/ros2_init.sh
+++ ros2_humble/ros2_init.sh
@@ -2,6 +2,8 @@
 
 # https://docs.ros.org/en/humble/Installation/Alternatives/Ubuntu-Development-Setup.html
 
+ubuntu_codename=$(. /etc/os-release && echo $UBUNTU_CODENAME)
+
 set -e
 
 sudo apt-get update
@@ -9,13 +11,13 @@ sudo apt-get upgrade -y
 sudo apt-get install -y apt-utils curl
 
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $ubuntu_codename main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install -s \
     python3-pip \
     ros-dev-tools \
-    | grep "^Inst" | awk '{print $2}' | sort -n > init-pkgs.txt
+    | grep "^Inst" | awk '{print $2}' | sort -n > ros2-init-installed-pkgs-$ubuntu_codename.txt
 sudo apt-get install -y \
     python3-pip \
     ros-dev-tools
