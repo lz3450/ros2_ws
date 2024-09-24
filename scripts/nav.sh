3a039706ac158a56054cba047be9cfc6866c0b3b
KZL <lz3450@outlook.com>
2024-09-24 15:49:14 -0500

    scripts

--- scripts/nav.sh
+++ scripts/nav.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
+pids=()
+
+sigint_handler() {
+    echo "Ctrl-C pressed! Sending SIGINT to all background processes..."
+    for pid in "${pids[@]}"; do
+        kill -INT "$pid"
+    done
+    # Optionally wait for all background jobs to exit
+    wait
+    exit 1
+}
+trap sigint_handler INT
+
 if [[ -z "$ROS_DISTRO" ]]; then
-    if [[ -f /etc/turtlebot4/setup.bash ]]; then
-        . /etc/turtlebot4/setup.bash
-    else
-        . ../../ros2_setup.sh
-    fi
+    . ../ros2_setup.sh
 fi
 
 ros2 launch turtlebot4_navigation slam.launch.py &
+pids+=($!)
 sleep 1
-ros2 launch turtlebot4_navigation localization.launch.py map:=ecss3220.yaml &
-sleep 1
+# ros2 launch turtlebot4_navigation localization.launch.py map:=$HOME/ecss3220.yaml &
+# pids+=($!)
+# sleep 1
 ros2 launch turtlebot4_navigation nav2.launch.py &
+pids+=($!)
 sleep 1
 ros2 launch turtlebot4_viz view_robot.launch.py &
+pids+=($!)
+
+wait
--- scripts/undock.sh
+++ scripts/undock.sh
@@ -4,7 +4,7 @@ if [[ -z "$ROS_DISTRO" ]]; then
     if [[ -f /etc/turtlebot4/setup.bash ]]; then
         . /etc/turtlebot4/setup.bash
     else
-        . ../../ros2_setup.sh
+        . ../ros2_setup.sh
     fi
 fi
 
