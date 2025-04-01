2cacdd14a5959fa8f3f511aaedb1213535e3109b
KZL <lz3450@outlook.com>
2025-04-01 00:56:23 -0500

    moveit2

--- moveit2/0-script-lib.sh
+++ moveit2/0-script-lib.sh
@@ -48,5 +48,5 @@ install_moveit2_dep_pkgs() {
     grep -v 'ros-' "$MOVEIT2_DEP_PKGS_FILE" | xargs sudo apt-get install -s \
         | grep "^Inst" | awk '{print $2}' | LC_ALL=C sort -n \
         > "$MOVEIT2_DEP_PKGS_TO_INSTALL_FILE"
-    xargs -a "$MOVEIT2_DEP_PKGS_TO_INSTALL_FILE" sudo apt-get install -y
+    xargs -a "$MOVEIT2_DEP_PKGS_FILE" sudo apt-get install -y
 }
