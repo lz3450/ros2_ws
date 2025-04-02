3623a497e5f254d1840e25cab838fdbe717b3973
KZL <lz3450@outlook.com>
2025-04-02 03:42:56 -0500

    moveit2

--- moveit2/1-update-deps.sh
+++ moveit2/1-update-deps.sh
@@ -14,7 +14,9 @@ umask 0022
 init_mixin
 
 get_moveit2_src
-# remove unnecessary packages
-rm -rf src/ros2_kortex
+sed -i -e "/gz_ros2_control/d" src/ros2_kortex/kortex_description/package.xml
+sed -i -e "/ros_gz_bridge/d" src/ros2_kortex/kortex_bringup/package.xml
+sed -i -e "/ros_gz_sim/d" src/ros2_kortex/kortex_bringup/package.xml
+sed -i -e '/include_directories(include)/a include_directories(SYSTEM ${stomp_INCLUDE_DIRS})' src/moveit2/moveit_planners/stomp/CMakeLists.txt
 
 update_moveit2_dep_pkgs
