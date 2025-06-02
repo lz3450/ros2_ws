5ccc82126563c027da0598844e057af4c315441a
RoboTrace <rt@s3lab.org>
2025-06-02 10:49:27 -0500

    moveit2: update deps.repos

--- moveit2/1-update-deps.sh
+++ moveit2/1-update-deps.sh
@@ -14,9 +14,15 @@ umask 0022
 init_mixin
 
 get_moveit2_src
-sed -i -e "/gz_ros2_control/d" src/ros2_kortex/kortex_description/package.xml
-sed -i -e "/ros_gz_bridge/d" src/ros2_kortex/kortex_bringup/package.xml
-sed -i -e "/ros_gz_sim/d" src/ros2_kortex/kortex_bringup/package.xml
-sed -i -e '/include_directories(include)/a include_directories(SYSTEM ${stomp_INCLUDE_DIRS})' src/moveit2/moveit_planners/stomp/CMakeLists.txt
+sed -i \
+    -e "/gz_ros2_control/d" \
+    src/ros2_kortex/kortex_description/package.xml
+sed -i \
+    -e "/ros_gz_bridge/d" \
+    -e "/ros_gz_sim/d" \
+    src/ros2_kortex/kortex_bringup/package.xml
+sed -i \
+    -e '/include_directories(include)/a include_directories(SYSTEM ${stomp_INCLUDE_DIRS})' \
+    src/moveit2/moveit_planners/stomp/CMakeLists.txt
 
 update_moveit2_dep_pkgs
