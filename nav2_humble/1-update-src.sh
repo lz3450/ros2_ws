e7f23510c1f39f18fd04673e9777a623fee27908
KZL <lz3450@outlook.com>
2025-10-01 11:43:14 -0500

    nav2/humble: build scripts

--- nav2_humble/1-update-src.sh
+++ nav2_humble/1-update-src.sh
@@ -14,5 +14,8 @@ umask 0022
 get_nav2_src
 
 rm -rf src/navigation2/nav2_system_tests
+rm -rf src/bond_core/{bond_core,bondpy,test_bond}
+rm -rf src/vision_opencv/{image_geometry,opencv_tests,vision_opencv}
+rm -rf src/diagnostics/{diagnostic_aggregator,diagnostic_common_diagnostics,diagnostic_remote_logging,diagnostics,self_test}
 
 echo "Successfully updated Nav2 source code and applied patches"
