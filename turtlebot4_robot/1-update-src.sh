737467e28f79546e644b90ae2c61e073bcf0b44c
KZL <lz3450@outlook.com>
2025-10-01 13:39:20 -0500

    tb4/robot: deps

--- turtlebot4_robot/1-update-src.sh
+++ turtlebot4_robot/1-update-src.sh
@@ -19,8 +19,9 @@ rm -rf src/foxglove-sdk/ros/src/foxglove_bridge
 rm -rf src/image_transport_plugins/{zstd_image_transport}
 rm -rf src/create3_sim/irobot_create_common/{irobot_create_common_bringup,irobot_create_control,irobot_create_nodes,irobot_create_toolbox}
 rm -rf src/create3_sim/{irobot_create_gazebo,irobot_create_ignition}
-rm -rf src/joystick_drivers/{joystick_drivers,ps3joy,sdl2_vendor,spacenav,wiimote,wiimote_msgs}
+rm -rf src/joystick_drivers/{joystick_drivers,ps3joy,spacenav,wiimote,wiimote_msgs}
 rm -rf src/imu_tools/{imu_complementary_filter,imu_filter_madgwick,imu_tools}
 rm -rf src/vision_msgs/vision_msgs_rviz_plugins
+rm -rf src/vision_opencv/{cv_bridge,opencv_tests,vision_opencv}
 
 echo "Successfully updated TurtleBot4 (robot) source code and applied patches"
