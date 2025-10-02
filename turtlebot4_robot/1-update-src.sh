61d94e354d92ec85e86fd06bdd25f6ac471a7970
KZL <lz3450@outlook.com>
2025-10-02 04:53:02 -0500

    tb4/robot: deps

--- turtlebot4_robot/1-update-src.sh
+++ turtlebot4_robot/1-update-src.sh
@@ -17,7 +17,7 @@ rm -rf src/create3_examples/{create3_coverage,create3_examples_msgs,create3_exam
 rm -rf src/depthai-ros/{depthai-ros,depthai_filters}
 rm -rf src/foxglove-sdk/ros/src/foxglove_bridge
 rm -rf src/image_transport_plugins/{zstd_image_transport}
-rm -rf src/create3_sim/irobot_create_common/{irobot_create_common_bringup,irobot_create_control,irobot_create_nodes,irobot_create_toolbox}
+rm -rf src/create3_sim/irobot_create_common/{irobot_create_common_bringup,irobot_create_nodes,irobot_create_toolbox}
 rm -rf src/create3_sim/{irobot_create_gazebo,irobot_create_ignition}
 rm -rf src/joystick_drivers/{joystick_drivers,ps3joy,spacenav,wiimote,wiimote_msgs}
 rm -rf src/imu_tools/{imu_complementary_filter,imu_filter_madgwick,imu_tools}
