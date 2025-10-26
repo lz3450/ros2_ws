3a3857931a68292022c3ca0fa2bccf4a87ce62a9
KZL <lz3450@outlook.com>
2025-10-26 10:46:50 -0500

    docker: run

--- docker/run.sh
+++ docker/run.sh
@@ -6,4 +6,4 @@
 docker run -d --rm -u root --name humble -h HUMBLE \
     --mount type=bind,source=/home/kzl/LFS,target=/home/kzl/LFS \
     --mount type=bind,source=/home/kzl/Projects/RoboGuard/rg_ws,target=/home/kzl/Projects/RoboGuard/rg_ws \
-    --mount type=bind,source=/home/kzl/Projects/ros2_ws,target=/home/kzl/Projects/ros2_ws humble:desktop
+    --mount type=bind,source=/home/kzl/Projects/ros2_ws,target=/home/kzl/Projects/ros2_ws humble:ros2dev
