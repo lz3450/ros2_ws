ed86d4e23cb40c74c26438db14c7de353d6dd11e
KZL <lz3450@outlook.com>
2025-10-27 20:02:19 -0500

    scripts: ros2 humble

--- scripts/ros2_humble/1-update-src.sh
+++ scripts/ros2_humble/1-update-src.sh
@@ -15,7 +15,18 @@ umask 0022
 ROS_DISTRO=humble
 . ../lib.sh
 
+srcdir="$ROS2_WS/src/ros2_humble"
+
+################################################################################
+rm -rf "$srcdir"
+
+################################################################################
 wget https://raw.githubusercontent.com/ros2/ros2/$ROS_DISTRO/ros2.repos -O ros2_humble.repos
-get_src ros2_humble.repos "$ROS2_WS/src/ros2_humble"
+get_src ros2_humble.repos "$srcdir"
 
 echo "Successfully fetched ROS2 $ROS_DISTRO source code"
+
+################################################################################
+get_dep_pkgs dep-pkgs.txt "$srcdir"
+
+echo "Successfully updated dependency package list"
