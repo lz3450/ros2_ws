286b55edbd6d5bf463084432ba4759bbd532364a
KZL <lz3450@outlook.com>
2026-02-16 00:32:28 -0600

    scripts: jazzy

---
+++ scripts/ros2_jazzy/1-update-src.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# update-src.sh
+#
+
+set -e
+set -o pipefail
+# set -u
+# set -x
+
+umask 0022
+
+################################################################################
+
+ROS_DISTRO=jazzy
+. ../lib.sh
+
+srcdir="$ROS2_WS/src/ros2_jazzy"
+
+################################################################################
+rm -rf "$srcdir"
+
+################################################################################
+wget https://raw.githubusercontent.com/ros2/ros2/$ROS_DISTRO/ros2.repos -O ros2_jazzy.repos
+get_src ros2_jazzy.repos "$srcdir"
+
+echo "Successfully fetched ROS2 $ROS_DISTRO source code"
+
+################################################################################
+get_dep_pkgs dep-pkgs.txt "$srcdir"
+
+echo "Successfully updated dependency package list"
