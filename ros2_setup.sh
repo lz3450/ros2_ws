af722889030e70c609941b7aa2f8828b5bffa760
KZL <lz3450@outlook.com>
2026-02-18 03:28:19 -0600

    setup

--- ros2_setup.sh
+++ ros2_setup.sh
@@ -1,19 +1,22 @@
 if [ -n "$BASH_VERSION" ]; then
-    export ROS2_WS="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1; pwd -P)"
+    export ROS2_WS="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
     ROS2_SHELL="bash"
 elif [ -n "$ZSH_VERSION" ]; then
-    export ROS2_WS="$(cd -- "$(dirname "${(%):-%x}")" > /dev/null 2>&1; pwd -P)"
+    export ROS2_WS="$(cd "$(dirname "${(%):-%x}")" && pwd)"
     ROS2_SHELL="zsh"
 else
     echo "Unsupported shell"
 fi
 echo "ROS2_WS=$ROS2_WS"
 
-# export ROS_DOMAIN_ID=77
-# echo "ROS_DOMAIN_ID=$ROS_DOMAIN_ID"
-# export RMW_IMPLEMENTATION="rmw_fastrtps_cpp"
+### Domain ID
+export ROS_DOMAIN_ID=77
+echo "ROS_DOMAIN_ID=$ROS_DOMAIN_ID"
+
+### RMW Implementation
+export RMW_IMPLEMENTATION="rmw_fastrtps_cpp"
 # export RMW_IMPLEMENTATION="rmw_cyclonedds_cpp"
-# echo "RMW_IMPLEMENTATION=$RMW_IMPLEMENTATION"
+echo "RMW_IMPLEMENTATION=$RMW_IMPLEMENTATION"
 
 ROS2_SETUP="$ROS2_WS/install/local_setup.$ROS2_SHELL"
 if [[ -f "$ROS2_SETUP" ]]; then
